d5a4848efdce5859958782f6b0ae4a563493eec8
HadiMh <hadih7899@gmail.com>
2019-01-20 22:22:53 +0330

    added dependencies and launcher icons successfully

--- app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/londonappbrewery/magiceightball/ExampleInstrumentedTest.java
@@ -9,6 +9,8 @@ import android.test.ApplicationTestCase;
 
 public class ExampleInstrumentedTest extends ApplicationTestCase<Application> {
     public ExampleInstrumentedTest() {
+
         super(Application.class);
+
     }
 }
